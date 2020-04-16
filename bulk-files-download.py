72db3b8bd6cb97ddd84f33c7b3dd747744b8de74
Adam Styles <mrfawn@gmail.com>
2020-04-16 20:19:49 +1000

    quick duplicate update

--- bulk-files-download.py
+++ bulk-files-download.py
@@ -31,36 +31,3 @@ def main(x):
 for i in range(len(urls)):
     print(urls[i])
     main(urls[i])
-import re
-import requests
-from requests.exceptions import RequestException
-
-# Array of URLS that will be downloaded
-urls = [
-    '',
-    '',
-    '',
-    #''
-]
-
-def main(x):
-
-    try:
-        with requests.get(x) as r:
-
-            fname = ''
-            if "Content-Disposition" in r.headers.keys():
-                fname = re.findall("filename=(.+)", r.headers["Content-Disposition"])[0]
-            else:
-                fname = x.split("/")[-1]
-
-            print(fname)
-            open(fname, 'wb').write(r.content)
-
-    except RequestException as e:
-        print(e)
-
-
-for i in range(len(urls)):
-    print(urls[i])
-    main(urls[i])
